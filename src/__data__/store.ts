e05af098b81271e9c6cfda6e2eb85e9e57c34692
Daniel <dan.usov82@gmail.com>
2023-11-05 11:56:40 +0300

    new conf

---
+++ src/__data__/store.ts
@@ -0,0 +1,17 @@
+import { combineReducers, configureStore } from '@reduxjs/toolkit';
+import { setupListeners } from '@reduxjs/toolkit/query';
+import { authApi } from './services/auth';
+
+const rootReducer = combineReducers({
+  [authApi.reducerPath]: authApi.reducer,
+});
+
+const apiMiddleware = [authApi.middleware];
+
+export const store = configureStore({
+  reducer: rootReducer,
+  middleware: (getDefaultMiddleware) =>
+    getDefaultMiddleware().concat(apiMiddleware),
+});
+
+setupListeners(store.dispatch);
