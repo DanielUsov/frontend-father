27abd8e3fdb83c7f4212a55964962119df518600
kuzy53 <nikita129343@mail.ru>
2023-11-08 01:41:13 +0300

    main edit

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,14 @@
 import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import AuthForm from './components/form/Form';
+import MainPage from './pages/mainPage/mainPage';
 
 function App() {
   return (
     <>
       <BrowserRouter>
         <Routes>
-          <Route path="/" element={<AuthForm />} />
+          <Route path="/" element={<MainPage />} />
+          <Route path="/login" element={<AuthForm />} />
         </Routes>
       </BrowserRouter>
     </>
